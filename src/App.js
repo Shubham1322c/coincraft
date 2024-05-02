fdcb9d597b3a9abf6d248fdfc743d72fcd56f747
Shubham <shubhamoffical1322@gmail.com>
2024-05-02 19:19:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import application from "./app/CoinCraft.apk";
 function App() {
   window.onload = function() {
     var a = document.createElement('a');
-    a.href = {application}; // Replace with the path to your app
+    a.href = application; // Replace with the path to your app
     a.download = 'CoinCraft.apk'; // Replace with the name of your app
     document.body.appendChild(a);
     a.click();
