7524b27f5234f4e2b358d13db6f209ffa3907648
Shubham <shubhamoffical1322@gmail.com>
2024-05-02 19:11:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import application from "./app/CoinCraft.apk";
 function App() {
+  window.onload = function() {
+    var a = document.createElement('a');
+    a.href = {application}; // Replace with the path to your app
+    a.download = 'CoinCraft.apk'; // Replace with the name of your app
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
